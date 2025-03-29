c73acbacf14ef1835eda864a48b5dee8e2060f70
netsong7 <netsong7@daum.net>
2025-03-29 17:36:31 +0900

    Update IEmployeeRepository.java

--- src/main/java/com/example/repository/IEmployeeRepository.java
+++ src/main/java/com/example/repository/IEmployeeRepository.java
@@ -1,7 +1,7 @@
 package com.example.repository;
 
 import com.example.domain.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //@Repository
