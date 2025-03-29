d5f010e2c2223a12c4aff8afefbc01fcc799070e
netsong7 <netsong7@daum.net>
2025-03-29 17:34:51 +0900

    Update Employee.java

--- src/main/java/com/example/domain/Employee.java
+++ src/main/java/com/example/domain/Employee.java
@@ -1,11 +1,11 @@
 package com.example.domain;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
 
 //@Entity
 //@Table(name = "employee")
