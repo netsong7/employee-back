c0efdb5968ef174d90cc1eb2be61656c90f1303f
netsong7 <netsong7@daum.net>
2025-03-29 17:35:36 +0900

    Update IEmployeeRepository.java

--- src/main/java/com/example/repository/IEmployeeRepository.java
+++ src/main/java/com/example/repository/IEmployeeRepository.java
@@ -4,6 +4,8 @@ import com.example.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
-}
+//@Repository
+//public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
+//}
+
+public interface IEmployeeRepository {}
